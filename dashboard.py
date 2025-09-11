e7cb592a865961ba0c2ad38394c2512ba65e3419
Lucas Dias Noronha <107411935+LUCASDNORONHA@users.noreply.github.com>
2025-09-10 22:23:58 -0300

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -36,6 +36,7 @@ st.header("🏆 Ranking Histórico")
 
 ranking_query = """
 SELECT p.nome AS Piloto,
+       p.nome AS Equipe,
        SUM(r.pontos) AS Pontos, 
        MIN(r.posicao) AS Melhor_Posicao, 
        MIN(r.melhor_volta) AS Melhor_Volta
